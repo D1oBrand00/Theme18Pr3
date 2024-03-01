ef5fd0b98dbe040a665711029c41fb85bf6d9209
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-03-01 13:24:41 +0200

    Update Program.cs

--- Theme18Pr3/Program.cs
+++ Theme18Pr3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,60 +10,156 @@ namespace Theme18Pr3
     {
         static void Main(string[] args)
         {
-            while (true)
+            bool t = true;
+            bool tt = true;
+            List<Figure> fig = new List<Figure>();
+            while (t)
             {
-                Console.Write($"Выберите фигуру(Прямоугольник,квадрат,треугольник, окружность) ");
-                string figure = Console.ReadLine();
-                switch (figure.ToLower())
+                Console.Write("Действия с фигурой:\nДобавить объект - add\nПросмотреть весь список - showall\nПросмотреть значение всех свойств объекта - show\nРедактировать объект - change\nУдалить объект - delete\n\n");
+                string action = Console.ReadLine();
+                switch (action.ToLower())
                 {
-                    case "прямоугольник":
-                        List<Figure> fig1 = new List<Figure>();
-                        fig1.Add(new Rectangle($"{figure}",5,5));
-                        foreach (var elem in fig1)
+                    case "add":
+                        while(tt)
+                        {
+                            Console.Write($"Какую добавить фигуру(Прямоугольник,квадрат,треугольник, окружность)\nВыйти - q\n");
+                            string figure = Console.ReadLine();
+
+                            switch (figure.ToLower())
+                            {
+                                case "прямоугольник":
+                                    fig.Add(Rectangle.Input());
+
+                                    break;
+                                case "квадрат":
+                                    fig.Add(Square.Input());
+                                    break;
+                                case "треугольник":
+                                    fig.Add(Triangle.Input());
+                                    break;
+                                case "окружность":
+                                    fig.Add(Circle.Input());
+                                    break;
+                                case "q":
+                                    tt = false;
+                                    break;
+                            }
+                        }
+                        tt = true;
+                        break;
+                    case "showall":
+                        foreach (var elem in fig)
                         {
                             elem.Print();
-
+                            Console.WriteLine();
                         }
                         break;
-                    case "квадрат":
-                        List<Figure> fig2 = new List<Figure>();
-                        fig2.Add(new Square($"{figure}", 6));
-                        foreach (var elem in fig2)
+                    case "show":
+                        while (tt)
                         {
-                            elem.Print();
+                            Console.Write($"Просмотреть значение всех свойств объектов (Прямоугольник,квадрат,треугольник, окружность)\nВыйти - q\n");
+                            string figure = Console.ReadLine();
+                            Console.Write($"Номер объекта ");
+                            int n = int.Parse(Console.ReadLine());
+                            switch (figure.ToLower())
+                            {
 
+                                case "прямоугольник":
+                                    fig[n].Print();
+  
+                                    
+                                    break;
+                                case "квадрат":
+                                    fig[n].Print();
+                                    break;
+                                case "треугольник":
+                                    fig[n].Print();
+                                    break;
+                                case "окружность":
+                                    fig[n].Print();
+                                    break;
+                                case "q":
+                                    tt = false;
+                                    break;
+                            }
                         }
+                        tt = true;
                         break;
-                    case "треугольник":
-                        List<Figure> fig3 = new List<Figure>();
-                        fig3.Add(new Triangle($"{figure}", 5,4,2));
-                        foreach (var elem in fig3)
+                    case "change":
+                        while (tt)
                         {
-                            elem.Print();
+                            Console.Write($"Редактировать значение любого из свойств объектов (Прямоугольник,квадрат,треугольник, окружность)\nВыйти - q\n");
+                            string figure = Console.ReadLine();
+                            Console.Write($"Номер объекта ");
+                            int n = int.Parse(Console.ReadLine());
+                            switch (figure.ToLower())
+                            {
 
+                                case "прямоугольник":
+                                    fig[n] = Rectangle.Input();
+
+                                    break;
+                                case "квадрат":
+                                    fig[n] = Square.Input();
+                                    break;
+                                case "треугольник":
+                                    fig[n] = Triangle.Input();
+                                    break;
+                                case "окружность":
+                                    fig[n] = Circle.Input();
+                                    break;
+                                case "q":
+                                    tt = false;
+                                    break;
+                            }
+                            
                         }
+                        tt = true;
                         break;
-                    case "окружность":
-                        List<Figure> fig4 = new List<Figure>();
-                        fig4.Add(new Circle($"{figure}", 4));     ;
-                        foreach (var elem in fig4)
+                    case "delete":
+                        while (tt)
                         {
-                            elem.Print();
+                            Console.Write($"Удалить любой из объектов (Прямоугольник,квадрат,треугольник, окружность)\nВыйти - q\n");
+                            string figure = Console.ReadLine();
+                            Console.Write($"Номер объекта ");
+                            int n = int.Parse(Console.ReadLine());
+                            switch (figure.ToLower())
+                            {
+
+                                case "прямоугольник":
+                                    fig.RemoveAt(n);
+
+                                    break;
+                                case "квадрат":
+                                    fig.RemoveAt(n);
+                                    break;
+                                case "треугольник":
+                                    fig.RemoveAt(n);
+                                    break;
+                                case "окружность":
+                                    fig.RemoveAt(n);
+                                    break;
+                                case "q":
+                                    tt = false;
+                                    break;
+                            }
 
                         }
+                        tt = true;
                         break;
-
-                    case "q":
-                        return;
                     default:
-                        Console.WriteLine("Я не знаю такой фигуры :/");
+                        t = false;
                         break;
-
+                        
 
 
                 }
-                Console.ReadKey();
-            }
+
+            }    
+            
+            Console.ReadKey();
+
+
         }
     }
 }
